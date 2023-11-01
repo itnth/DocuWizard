def8e8dd54a50ed193a2e1c164c367bc5013cca8
itnth <it.nth02@gmail.com>
2023-11-01 17:07:20 +0700

    checkin custom fonts

--- src/components/DocuWizardEditor.vue
+++ src/components/DocuWizardEditor.vue
@@ -4,13 +4,3 @@
     </main>
 </template>
 <script src="./DocuWizardEditor.ts" lang="ts"/>
-<style scoped>
-@media (min-width: 1024px) {
-  #sample {
-    display: flex;
-    flex-direction: column;
-    place-items: center;
-    width: 100%;
-  }
-}
-</style>
