adcd61f29d73059df2e36e21b6ad93417431560c
itnth <it.nth02@gmail.com>
2023-11-02 11:30:36 +0700

    checkin code

--- src/components/DocuWizardEditor.vue
+++ src/components/DocuWizardEditor.vue
@@ -1,6 +1,33 @@
-<template>
+<!-- <template>
     <main id="sample">
         <Editor :init="editorInit" />
     </main>
+</template> -->
+<template>
+    <a-tabs v-model:activeKey="activeKey" class="p-2" >
+        <a-tab-pane key="1">
+            <template #tab>
+                <span>
+                    <FormOutlined />
+                    Trình soạn thảo
+                </span>
+            </template>
+            <main id="sample">
+                <Editor :init="editorInit" />
+            </main>
+        </a-tab-pane>
+        <a-tab-pane key="2">
+            <template #tab>
+                <span>
+                    <SolutionOutlined />
+                    Giới thiệu
+                </span>
+            </template>
+            Tab 2
+        </a-tab-pane>
+    </a-tabs>
 </template>
 <script src="./DocuWizardEditor.ts" lang="ts"/>
+
+
+
